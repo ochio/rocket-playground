cee09f0e1a1aad307541a142ed12754149bc6294
ochi <ocos54ce@gmail.com>
2024-08-16 20:22:42 +0900

    dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use dotenvy::dotenv;
+use std::env;
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> String {
+    let token = env::var("TOKEN").expect("FOO not set");
+    let url = "https://www.rust-lang.org";
+    let contents = reqwest::get(url).await.unwrap().text().await;
+    format!("{}", contents.unwrap())
 }
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().expect(".env file not found");
     rocket::build().mount("/", routes![index])
 }
